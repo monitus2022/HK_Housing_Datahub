e8857de6b185c8dca9ea91531182986959f9fe52
monitus2022 <monitus202201@proton.me>
2025-10-19 03:51:26 +0800

    [#2] Added util to read txt file

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 def cookie_str_to_dict(cookie_str: str) -> dict[str, str]:
     """
     Convert a cookie string to a dictionary.
@@ -10,3 +12,11 @@ def cookie_str_to_dict(cookie_str: str) -> dict[str, str]:
             key, value = item.split("=", 1)
             cookies[key.strip()] = value.strip()
     return cookies
+
+def txt_to_list(file_path: str, dtype: Union[str, int, float]=str) -> list[Union[str, int, float]]:
+    """
+    Read a text file and return a list of lines.
+    Each line is stripped of leading/trailing whitespace.
+    """
+    with open(file_path, "r", encoding="utf-8") as f:
+        return [dtype(line.strip()) for line in f if line.strip()]
